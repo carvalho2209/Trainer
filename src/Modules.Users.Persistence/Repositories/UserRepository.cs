209ffe17a0ecc90bf7d587e479feac1727beeb75
Alexandre Carvalho <carvalho2209@hotmail.com>
2024-01-17 16:12:35 -0800

    starting set up result pattern and added docker compose

---
+++ src/Modules.Users.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Application.ServiceLifetimes;
+using Microsoft.EntityFrameworkCore;
+using Modules.Users.Domain.Users;
+using Shared.Results;
+
+namespace Modules.Users.Persistence.Repositories;
+
+internal sealed class UserRepository : IUserRepository, IScoped
+{
+    private readonly UsersDbContext _context;
+
+    public UserRepository(UsersDbContext context) => _context = context;
+
+    public async Task<Result<User>> GetByIdAsync(UserId id, CancellationToken cancellationToken = default) =>
+        Result.Create(await _context.Set<User>()
+            .Include(user => user.Roles).FirstOrDefaultAsync(user => user.Id == id, cancellationToken));
+
+    public async Task<Result> IsEmailUniqueAsync(string email, CancellationToken cancellationToken = default) =>
+        Result.Create(!await _context.Set<User>()
+            .AnyAsync(user => user.Email == email, cancellationToken));
+
+    public void Add(User user)
+    {
+        _context.Set<User>().Add(user);
+
+        _context.AttachRange(user.Roles);
+    }
+}
